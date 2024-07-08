0cda4f9c79d01259b45a9a9426ce09ace5d55f40
S!R X <111647198+SIR-X@users.noreply.github.com>
2024-07-08 16:50:26 +0330

    Fix a bug with get_session (#76)
    * Fix a bug
    
    InputBotInlineMessageID's dc_id being the same with Client's dc_id. Closes #74
    
    ---------
    
    Co-authored-by: KurimuzonAkuma <31959970+KurimuzonAkuma@users.noreply.github.com>

--- pyrogram/methods/messages/inline_session.py
+++ pyrogram/methods/messages/inline_session.py
@@ -36,6 +36,9 @@ async def get_session(client: "pyrogram.Client", dc_id: int) -> Session:
 
         await session.start()
 
+        if dc_id == await client.storage.dc_id():
+            return session
+        
         for _ in range(3):
             exported_auth = await client.invoke(
                 raw.functions.auth.ExportAuthorization(
